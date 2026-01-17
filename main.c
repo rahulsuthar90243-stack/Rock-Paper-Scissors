46755b5c133df6940a940978f906496928fc2088
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-17 11:47:17 +0530

    add while loop and witch statement modifiying

--- main.c
+++ main.c
@@ -4,20 +4,21 @@ int main(){
     int choice;
     printf("\n\n--Welcome Rock Paper Scissors\n");
     print_menu();
-    printf("\nPlease, enter oprion: ");
-    scanf("%d", &choice);
 
-    switch (choice){
-    case 1:
-        break;
+    while (1)
+    {
+        switch (choice)
+        {
+            printf("\nPlease, enter oprion: ");
+            scanf("%d", &choice);
+        case 1:
+            break;
 
-    case 0:
-        return 0;    
-    
-    default: if(choice < 0 && choice > 1){
-        printf("Invalide choice!!!");
+        case 0:
+            return 0;
+
+        default:
+                printf("Invalide choice!!!");
+        }
     }
-        break;
-    }
-
 }
