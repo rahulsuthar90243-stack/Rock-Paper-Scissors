de325c4b3e9dbcb5007f42ae930632e675daf7a1
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-18 14:30:45 +0530

    show user and compter chocie function definition

---
+++ show_choice.c
@@ -0,0 +1,11 @@
+#include "game.h"
+
+int show_choice(int choice){
+
+    switch (choice){
+    case Rock: printf("Rock");break;
+    case Paper: printf("Paper");break;
+    case Scissors: printf("Scissors");break;
+    }
+
+}
