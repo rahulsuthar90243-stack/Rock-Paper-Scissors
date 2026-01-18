cb25acf2619b926b9d22768882efdd9cadb4f1f1
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-18 10:15:19 +0530

    update menu option

--- print_menu.c
+++ print_menu.c
@@ -2,7 +2,9 @@
 
 
 void print_menu(){
-
+    printf("\n ---------------------------");
+    printf("\n|    ROCK PAPER SCISSORS    |");
+    printf("\n ---------------------------");
     printf("\n[1]. Play Game");
     printf("\n[0]. Exit Game");
 }
