3f01e0d6d89911eafe793c749ade47d47d3bff1d
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-19 11:34:27 +0530

    Add color to console output

--- game.h
+++ game.h
@@ -12,6 +12,13 @@ extern int user_score;
 extern int comp_score;
 extern int game_draw;
 
+const char* COLOR_END = "\033[0m";
+const char* RED ="\033[1;31m"; 
+const char* GREEN ="\033[1;32m";
+const char* YELLOW ="\033[1;33m"; 
+const char* MAGENTA = "\033[0;35m"; 
+const char* CYAN = "\033[0;36m"; 
+
 void print_menu();
 int get_user_choice();
 int get_comp_choice();
